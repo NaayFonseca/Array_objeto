0728ccd36478d31abd51d3460a6bd7d394e25be8
NAYRA FONSECA DOS SANTOS <nayrasantos0842@digitalcollege.internal>
2025-06-10 20:22:03 -0300

    array

---
+++ array.js
@@ -0,0 +1,44 @@
+//lista de itens 
+
+// 1.criar
+let nomes = ["ana","Bia", "Carlos", "Daniel" ]
+let carro = []
+let idades = [12, 25, 24]
+
+const cidadeArgentina = [
+
+]
+
+//le
+//console.log('{tamanho: $cidadeArgentina}');
+
+// 2.limpar
+console.log(idades)
+// adicionar 
+//console.log(carros);
+
+//elementos
+//carros.push("fusca");
+//console.log(carros);
+// remover elementos
+/*console.log(nomes)
+nomes.pop()
+console.log(nomes)
+nomes.shift()
+console.log(nomes);
+console.log(nomes[1]);
+let nomeProprio = `${nomes[1]} ${nomes[2]}`
+console.log(nomeProprio.toUppercase);*/
+
+
+
+
+//buscar elementos(filtrar)
+ // percorrer os itens
+ let frutas = ['maçã', 'banana', 'manga', 'abacaxi', 'kiwi'];
+ frutas.forEach(function (fruta) {
+    console.log(fruta);
+
+ })
+
+ 
